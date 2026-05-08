a32017cddd2a7aac07920a6f870862f919895600
Aliff Mateus Pereira Da Silva <edumay935@gmail.com>
2026-05-08 11:44:33 -0300

    Create ex01.py

---
+++ pratica_02/ex01.py
@@ -0,0 +1,10 @@
+# Exercício 1: Classificação de Número
+numero = int(input("Digite um número inteiro: "))
+
+if numero > 0:
+    print("O número é Positivo.")
+elif numero < 0:
+    print("O número é Negativo.")
+else:
+    print("O número é Zero.")
+  
