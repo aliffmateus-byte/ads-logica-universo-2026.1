d4a7b81261566f3f1cb4989df5ba06c92db5f938
Aliff Mateus Pereira Da Silva <edumay935@gmail.com>
2026-05-08 11:40:17 -0300

    Create ex14.py

---
+++ pratica_02/ex14.py
@@ -0,0 +1,9 @@
+# Exercício 14: Refatoração (While para For)
+# Refatorando o exercício 13 que usava while para uma estrutura for
+print("Contagem regressiva com For:")
+for i in range(10, 0, -1):
+    print(i)
+
+# Explicação: O código com 'for' fica mais simples pois o controle 
+# do início, fim e passo (decremento) é feito em uma única linha.
+
