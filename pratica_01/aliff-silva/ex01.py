6d6bb41aeb97ffb0ecc5edbdf9c00ef87c394787
Aliff Mateus Pereira Da Silva <edumay935@gmail.com>
2026-05-08 03:02:11 -0300

    Create ex01.py

---
+++ pratica_01/aliff-silva/ex01.py
@@ -0,0 +1,6 @@
+nome = input("Digite o nome: ")
+ano_nascimento = int(input("Digite o ano de nascimento: "))
+altura = float(input("Digite a altura: "))
+idade = 2026 - ano_nascimento 
+print(f"Olá, {nome}! Você tem {idade} anos e sua altura é de {altura}m. Registro concluído.")
+
