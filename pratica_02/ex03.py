8b5acf268bd4aab9aed8e6d5c8993a32b34601a1
Aliff Mateus Pereira Da Silva <edumay935@gmail.com>
2026-05-08 11:29:38 -0300

    Create ex03.py

---
+++ pratica_02/ex03.py
@@ -0,0 +1,7 @@
+# Exercício 3: Par ou Ímpar
+print("Verificando paridade de 1 a 20:")
+for i in range(1, 21):
+    if i % 2 == 0:
+        print(f"{i} é par.")
+    else:
+        print(f"{i} é ímpar.")
